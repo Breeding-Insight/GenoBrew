34beb927d61621a99f03b8673b117cdbdc0a03f9
Cristianetaniguti <chaytaniguti@gmail.com>
2026-04-22 12:58:13 -0400

    run_app to NAMESPACE by hand

--- R/run_app.R
+++ R/run_app.R
@@ -4,20 +4,17 @@
 #' See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
 #'
-#' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 #'
-run_app <- function(
-    onStart = NULL,
-    options = list(),
-    enableBookmarking = NULL,
-    uiPattern = "/",
-    ...
-) {
-
-  with_golem_options(
-    app = shinyApp(
+#' @export
+run_app <- function(onStart = NULL,
+                    options = list(),
+                    enableBookmarking = NULL,
+                    uiPattern = "/",
+                    ...) {
+  golem::with_golem_options(
+    app = shiny::shinyApp(
       ui = app_ui,
       server = app_server,
       onStart = onStart,
